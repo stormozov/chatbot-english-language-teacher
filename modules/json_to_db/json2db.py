04a3d83e782d170443c88324da2d07bf472a4121
Sergey Tormozov <web@stormozov.ru>
2024-08-06 17:31:00 +0300

    fix(json-to-db): The "import_json_data_to_db" function has been redesigned
    - A "try-excert" block was added
    - A more efficient way of checking for existing objects was used
    - A transaction was used

--- modules/json_to_db/json2db.py
+++ modules/json_to_db/json2db.py
@@ -6,25 +6,51 @@ def import_json_data_to_db(session, path: str) -> None:
     """Upload data from a JSON file to the database"""
     data = read_json_file(path)
 
-    for category_title, category_content in data['categories'].items():
-        category = session.query(Category).filter_by(title=category_title).first()
+    # Create dictionaries to keep track of existing objects
+    categories = {category.title: category for category in session.query(Category).all()}
+    words = {word.word: word for word in session.query(Word).all()}
+    translations = {(word.word, translation.translation): translation
+                    for word, translation in session.query(Word, TranslatedWord).join(TranslatedWord).all()}
 
-        if category is None:
-            category = Category(title=category_title)
-            session.add(category)
+    try:
+        for category_title, category_content in data['categories'].items():
+            category = categories.get(category_title)
 
-        for word_data in category_content['words']:
-            word = session.query(Word).filter_by(word=word_data['word']).first()
+            if category is None:
+                category = Category(title=category_title)
+                categories[category_title] = category
+                session.add(category)  # Add the category to the session
+            else:
+                # Merge the existing category with the new one
+                category.title = category_title
 
-            if word is None:
-                word = Word(word=word_data['word'], category=category)
-                session.add(word)
+            for word_data in category_content['words']:
+                word = words.get(word_data['word'])
 
-            translation = session.query(TranslatedWord).filter_by(word=word,
-                                                                  translation=word_data['translation']).first()
+                if word is None:
+                    word = Word(word=word_data['word'], category=category)
+                    words[word_data['word']] = word
+                    session.add(word)  # Add the word to the session
+                else:
+                    # Merge the existing word with the new one
+                    word.word = word_data['word']
+                    word.category = category
 
-            if translation is None:
-                translation = TranslatedWord(translation=word_data['translation'], word=word)
-                session.add(translation)
+                translation_key = (word_data['word'], word_data['translation'])
+                translation = translations.get(translation_key)
 
-    session.commit()
+                if translation is None:
+                    translation = TranslatedWord(translation=word_data['translation'], word=word)
+                    translations[translation_key] = translation
+                    session.add(translation)  # Add the translation to the session
+                else:
+                    # Merge the existing translation with the new one
+                    translation.translation = word_data['translation']
+                    translation.word = word
+
+        session.commit()
+
+    except Exception as e:
+        # Roll back the changes if something goes wrong
+        session.rollback()
+        raise e
